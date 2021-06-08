81b3fbf8f859beda2ae944ed5081ada2059d20e0
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2021-06-08 11:56:13 -0500

    Made changes for the cluster testing

--- patterntest.sh
+++ patterntest.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 #Java
-echo Java testing
-cd ~/patternlets/patternlets/java-OpenMPI/00.spmd/ && make 2> err.txt > /dev/null
+echo "Java testing"
+cd ~/CSinParallel/Patternlets/java-OpenMPI/00.spmd/ && make 2> err.txt > /dev/null
 ./run 2>> ~/err.txt 1> ~/outtest.txt
 
 # Pthreads
-echo Pthread Testing
-cd ~/patternlets/patternlets/pthreads/07.barrier/ && make 2>> err.txt > /dev/null
+echo "Pthread Testing"
+cd ~/CSinParallel/Patternlets/pthreads/07.barrier/ && make 2>> err.txt > /dev/null
 ./barrier 2>> ~/err.txt 1>> ~/outtest.txt
 
 # MPI
-echo MPI testing
-cd ~/patternlets/patternlets/MPI/01.masterWorker/ && make 2>> err.txt > /dev/null 
+echo "MPI testing"
+cd ~/CSinParallel/Patternlets/MPI/01.masterWorker/ && make 2>> err.txt > /dev/null 
 mpirun -np 4 ./masterWorker 2>> ~/err.txt 1>> ~/outtest.txt
 
 #mpi4py
-echo mpi4py testng
-cd ~/patternlets/patternlets/mpi4py && python3 run.py 00spmd.py 4 2>>err.txt >> ~/outtest.txt
+echo "mpi4py testng"
+cd ~/CSinParallel/Patternlets/mpi4py && python3 run.py 00spmd.py 4 2>>err.txt >> ~/outtest.txt
 
 #hybrid-MPI+OpenMP
-echo hybrid testing
-cd ~/patternlets/patternlets/hybrid-MPI+OpenMP/01.spmd2 && make 2> err.txt > /dev/null
+echo "hybrid testing"
+cd ~/CSinParallel/Patternlets/hybrid-MPI+OpenMP/01.spmd2 && make 2> err.txt > /dev/null
 mpirun -np 4 ./spmd2 2>> ~/err.txt 1>> ~/outtest.txt
 
 #openMP
-echo openMP testing
-cd ~/patternlets/patternlets/openMP/08.reduction && make 2> err.txt > /dev/null
+echo "openMP testing"
+cd ~/CSinParallel/Patternlets/openMP/08.reduction && make 2> err.txt > /dev/null
 ./reduction 2>> err.txt 1>> ~/outtest.txt
 
 
@@ -34,7 +34,6 @@ cd ~/patternlets/patternlets/openMP/08.reduction && make 2> err.txt > /dev/null
 patternletsWork=true
 
 # Search for lines in ~/outtest.txt
-
 while IFS= read -r line
 do
   if ! grep "$line" ~/outtest.txt > /dev/null
@@ -42,18 +41,19 @@ do
     echo -e "\e[1;31mFOLLOWING LINE NOT DETECTED: $line \e[0m"
     patternletWork=false
   fi
-done < ~/CURI2021-Raspberry-Pi/testcheck.txt
+done < testcheck.txt
 
+#Output if all of the Patternlets worked
 if $patternletWork 
 then
   echo -e "\e[1;32mPatternlet testing showed no errors \e[0m"
-  #rm outtest.txt
-  #rm err.txt
+  rm ~/outtest.txt
+  rm ~/err.txt
 else
   echo -e "\e[1;33mTESTING RESULTED IN ERROR\e[0m"
 fi
 
-
+# In case any of the Patternlets didn't work
 if ! $patternletWork
 then
   if [ ! -s ~/err.txt ]
@@ -68,3 +68,46 @@ then
   fi
 fi
 
+#Presenting option to run a cluster (If anything other than Y or YES are submitted (NOT case-sensitive, this part is skipped)
+echo "Would you like to test a cluster? [y/N]"
+read response
+response=${response^^}
+if [ $responce -eq "Y" ] || [ $responce -eq "YES" ]
+then 
+  #creating the head-node and the host file
+  echo "Head node and hostfile configuration"
+  sudo head-node
+  soc-mpisetup
+  
+  #Testing Cluster on MPI/00.spmd patternlet
+  echo "MPI cluster testing"
+  cd ~/CSinParallel/Patternlets/MPI/00.spmd/ && make 2>> err.txt > /dev/null 
+  mpirun -hostfile ~/hostfile -np 12 ./spmd 2>> ~/err.txt 1>> ~/outtest.txt
+  
+  #checking for desired output
+  clusterWork=true
+  while IFS= read -r line
+  do
+    if ! grep "$line" ~/outtest.txt > /dev/null
+    then
+      echo -e "\e[1;31mFOLLOWING LINE NOT DETECTED: $line \e[0m"
+      clusterWork=false
+    fi
+  done < clustercheck.txt
+  
+  #reverting head node to worker node
+  echo "Reverting head-node to worker-node"
+  sudo worker-node
+  
+  #Output
+  if $clusterWork 
+  then
+    echo -e "\e[1;32mCluster testing showed no errors \e[0m"
+    rm ~/outtest.txt
+    rm ~/err.txt
+  else
+    echo -e "\e[1;32mTESTING THE CLUSTER RESULTED IN ERROR\e[0m"
+  fi
+else
+  echo -e "\e[1;34mCluster head node not tested in this script\e[0m"
+fi
