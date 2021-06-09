775bf5071fec91b184687d6c4f7000a5c2d1c631
Pi <pi@8GBPI>
2021-06-09 18:57:51 -0500

    some minor change

--- mytest.sh
+++ mytest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/patternlets/patternlets
+cd ~/CSinParallel/Patternlets
 cd mpi4py
 
 #Testing mpi4py
@@ -16,10 +16,10 @@ done
 #Testing MPI
 echo
 echo Testing MPI
-cd ~/patternlets/patternlets/MPI
+cd ~/CSinParallel/Patternlets/MPI
 directory2='00.spmd 01.masterWorker 02.parallelLoop-equalChunks 03.parallelLoop-chunksOf1 05.messagePassing 05.messagePassing 07.messagePassing3 08.broadcast 09.broadcastUserInput 10.broadcastSendReceive 11.broadcast2 12.reduction 13.reduction2 14.scatter 15.gather 16.barrier 17.barrier+Timing 18.reduce+Timing 19.sequenceNumbers 20.parallelLoopAdvanced 21.broadcast+ParallelLoop 22.scatterLoopGather 23.scatterV+gatherV'
 for mod in $directory2; do
-	cd ~/patternlets/patternlets/MPI/$mod
+	cd ~/CSinParallel/Patternlets/MPI/$mod
 	cFile=$(ls . | grep *.c)
 	mpicc -Wall -ansi -pedantic -std=c99 $cFile -o test > out.txt 2> templog.txt
 	mpirun -np 4 ./test > out.txt 2> templog.txt
@@ -29,24 +29,24 @@ done
 #Testing Java
 echo
 echo Testing Java-OpenMPI
-cd ~/patternlets/patternlets/java-OpenMPI
+cd ~/CSinParallel/Patternlets/java-OpenMPI
 directory3='00.spmd 01.masterWorker 02.parallelLoop-equalChunks 03.parallelLoop-chunksOf1 05.messagePassing 06.messagePassing2 07.messagePassing3 08.barrier 09.broadcast 10.broadcastUserInput 11.sendReceivePseudoBroadcast 12.broadcast2 13.reduction 14.reduction2 15.scatter 16.gather 17.barrier+timing 18.reduce+timing 19.sequenceNumbers 20.parallelLoopAdvanced 21.broadcastLoopGather 22.scatterLoopGather 23.scatterV+gatherV'
 for dir in $directory3; do
 	cd $dir
 	make > out.txt > templog.txt
 	./run 2 -f -z -y > out.txt 2> templog.txt
 	source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt $dir
-	cd ~/patternlets/patternlets/java-OpenMPI
+	cd ~/CSinParallel/Patternlets/java-OpenMPI
 done
 echo
 #Testing hyhrid
 echo Testing hybrid
-cd ~/patternlets/patternlets/hybrid-MPI+OpenMP/00.spmd
+cd ~/CSinParallel/Patternlets/hybrid-MPI+OpenMP/00.spmd
 make > out.txt
 mpirun -np 4 ./spmd > out.txt 2> templog.txt
 source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt 00.spmd
 
-cd ~/patternlets/patternlets/hybrid-MPI+OpenMP/01.spmd2 
+cd ~/CSinParallel/Patternlets/hybrid-MPI+OpenMP/01.spmd2 
 make > out.txt
 mpirun -np 4 ./spmd2 > out.txt 2> templog.txt
 source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt 01.spmd2
@@ -57,7 +57,7 @@ echo
 echo Testing OpenMP
 directory4='00.forkJoin 01.forkJoin2 02.spmd 03.spmd2 04.barrier 06.parallelLoop-equalChunks 07.parallelLoop-chunksOf1 08.reduction 09.reduction-userDefined 10.parallelLoop-dynamicSchedule 11.private 12.mutualExclusion-atomic 13.mutualExclusion-critical 14.mutualExclusion-critical2 15.mutualExclusion-critical3 16.sections'
 for mod in $directory4;do
-	cd ~/patternlets/patternlets/openMP/$mod
+	cd ~/CSinParallel/Patternlets/openMP/$mod
 	#pwd
 	cFile=$(ls . | grep *.c)
 	gcc -Wall -ansi -pedantic -std=c99 $cFile -o test -fopenmp > out.txt 2> templog.txt
@@ -70,7 +70,7 @@ echo
 echo Testing pthread
 directory5='00.forkJoin 01.forkJoin2 02.forkJoin3 03.forkJoin4 04.forkJoin5 05.forkJoin6 06.mutualExclusion 07.barrier'
 for mod in $directory5;do
-	cd ~/patternlets/patternlets/pthreads/$mod
+	cd ~/CSinParallel/Patternlets/pthreads/$mod
 	#pwd
 	cFile=$(ls . | grep *.c)
 	gcc -Wall -ansi -pedantic -std=c99 $cFile -o test -lpthread > out.txt 2> templog.txt
@@ -78,12 +78,12 @@ for mod in $directory5;do
 	source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt $mod	 
 done
 
-cd ~/patternlets/patternlets/pthreads/08.sharedQueue
+cd ~/CSinParallel/Patternlets/pthreads/08.sharedQueue
 make > out.txt 2> templog.txt
 ./producerConsumer 4 4 4 > out.txt 2> templog.txt
 source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt 08.shareQueue
 
-#cd ~/patternlets/patternlets/pthreads/04.forkJoin5
-#make > out.txt 2> templog.txt
-#./forkJoin5 4 > out.txt 2> templog.txt
-#source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt 04.forkJoin5 
+cd ~/CSinParallel/Patternlets/pthreads/04.forkJoin5
+make > out.txt 2> templog.txt
+./forkJoin5 4 > out.txt 2> templog.txt
+source ~/CURI2021-Raspberry-Pi/emptyCheck.sh templog.txt 04.forkJoin5 
